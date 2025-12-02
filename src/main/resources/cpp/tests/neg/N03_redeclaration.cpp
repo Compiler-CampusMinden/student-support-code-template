788fd7ed31639416be4cdcf710f9c617f2bc32a3
Carsten Gips <cagix@hsbi.de>
2025-12-02 17:39:08 +0100

    add negative tests for references (init)

--- src/main/resources/cpp/tests/neg/N03_redeclaration.cpp
+++
@@ -1,9 +0,0 @@
-int main() {
-    int b = 42;
-    int &bb = b;
-
-    // Re-Definition darf nicht akzeptiert werden
-    int &b = bb;
-
-    return 0;
-}
--- src/main/resources/cpp/tests/neg/N04_redeclaration.cpp
+++
@@ -1,9 +0,0 @@
-int main() {
-    char c = 'c';
-    char &cc = c;
-
-    // Re-Definition darf nicht akzeptiert werden
-    char &c = cc;
-
-    return 0;
-}
